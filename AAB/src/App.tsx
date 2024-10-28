706f8b242b61e2752120c226b8572268ce52af98
Noah <no852982@ucf.edu>
2024-10-28 17:33:51 -0400

    Update App.tsx

--- AAB/src/App.tsx
+++ AAB/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home'; 
 import Products from './pages/Products'; 
- import About from './pages/About'; 
+import About from './pages/About';
+import Cart from './pages/Cart';  
 import './App.css'
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/About" element={<About />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
     </Router>
   );
