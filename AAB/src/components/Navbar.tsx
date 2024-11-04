e95baa27d6831cae385fadc87ae9ec9ca12d1986
Randy Nguyen <randy22nguyen@gmail.com>
2024-11-03 23:01:09 -0500

    Added navbar
    Added basic navbar at the top of each page (it will be the top on every page once each page is actually filled) css changes will be made to it once we decide a bit more on the design

--- AAB/src/components/Navbar.tsx
+++ AAB/src/components/Navbar.tsx
@@ -2,7 +2,20 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <h1> Navbar </h1>
+        <nav className = "nav">
+            <a href = "/" className = "home"> Average at Best</a>
+            <ul>
+                <li>
+                    <a href = "/Products">Products</a>
+                </li>
+                <li>
+                    <a href = "/About">About</a>
+                </li>
+                <li>
+                    <a href = "/Cart">Cart</a>
+                </li>
+            </ul>
+        </nav>
     );
 };
 
