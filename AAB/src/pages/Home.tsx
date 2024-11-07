9b3445f43fa1950f34281e4909fd3bcd803e07c1
Katelyn Campbell <kcgarnet121@gmail.com>
2024-11-07 15:45:07 -0500

    More work on Home Page
    Adding about section

--- AAB/src/pages/Home.tsx
+++ AAB/src/pages/Home.tsx
@@ -1,4 +1,9 @@
-import './Home.css'; 
+import './Home.css';
+
+//icons for about section
+import compost from '../assets/icons/compost.png'; 
+import globe from '../assets/icons/globe.png';
+import sprint from '../assets/icons/sprint.png';
 
 const Home = () => {
   return (
@@ -22,10 +27,37 @@ const Home = () => {
       <section className="about-section">
         <h2 className="about-title">About Us</h2>
         <p className="about-text">
-          We are a brand dedicated to making running accessible and enjoyable for everyone. 
-          Whether you're a seasoned marathoner or a casual jogger, we provide the gear and 
-          resources to help you succeed on your fitness journey.
+        At Average at Best, we started with a simple idea: running is for everyone. Whether you're starting your journey
+         or simply looking for comfortable, affordable gear to keep up with your daily run, we’re here for you.
+         We know that the path to becoming the best version of yourself isn't about perfection, but persistence. 
+         Our apparel is designed to keep you comfortable, inspired, and supported every step of the way—no matter where your feet take you.
+          We're here to celebrate the everyday runner, the ones who believe that running is more about the journey than the destination. 
+          So, whether you're setting new personal records or just getting started, we're with you every step of the way. 
+          Because, at Average at Best, we believe every runner deserves the best.
         </p>
+
+        {/* new sections with icons */}
+        <section className="features">
+        <div className="feature">
+            <div className="feature-icon-wrapper">
+                <img src={sprint} alt="Sprint Icon" className="feature-icon" />
+            </div>
+            <h3>Performance</h3>
+        </div>
+        <div className="feature">
+            <div className="feature-icon-wrapper">
+                <img src={globe} alt="Globe Icon" className="feature-icon" />
+            </div>
+            <h3>Community</h3>
+        </div>
+        <div className="feature">
+            <div className="feature-icon-wrapper">
+                <img src={compost} alt="Compost Icon" className="feature-icon" />
+            </div>
+            <h3>Quality</h3>
+        </div>
+        </section>
+
       </section>
     </>
   );
