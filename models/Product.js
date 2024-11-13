834997e648b74ba49c0c40ae329958a29f34f163
noahf909 <112297507+noahf909@users.noreply.github.com>
2024-11-13 11:25:48 -0500

    add api stuff to server for flutter devs

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
+  _id: String,
   name: String,
   price: Number,
   description: String,
-  quantity: Number
+  quantity: Number,
+  frontImageUrl: String,
+  backImageUrl: String,
+  sizes: [
+    {
+      size: String,
+      quantity: Number,
+      _id: String
+    },  
+  ], 
 });
 
 module.exports = mongoose.model('Product', productSchema);
