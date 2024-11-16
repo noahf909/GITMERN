b2a74926d377109eaed80c9d18272e5730833b3a
Katelyn Campbell <kcgarnet121@gmail.com>
2024-11-16 15:45:02 -0500

    Navbar and Footer
    removing blue links in nav bar & style changes. Adding footer component

--- AAB/src/App.tsx
+++ AAB/src/App.tsx
@@ -11,23 +11,27 @@ import './App.css'
 import Register from './pages/Register';
 import Checkout from './pages/Checkout'; 
 import SignIn from './pages/SignIn';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     // Wrap the entire app in CartProvider to provide cart data across ALL components 
     <CartProvider>
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} /> {/* Dynamic route */}
-          <Route path="/About" element={<About />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Checkout" element={<Checkout />} /> 
-          <Route path="/SignIn" element={<SignIn />} />
-        </Routes>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} /> {/* Dynamic route */}
+            <Route path="/About" element={<About />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Checkout" element={<Checkout />} /> 
+            <Route path="/SignIn" element={<SignIn />} />
+          </Routes>
+        </div>
+        <Footer />
       </Router>
     </CartProvider>
   );
