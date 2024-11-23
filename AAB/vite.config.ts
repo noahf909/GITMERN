5faec61a2b62b17502c1f05a3446a687755266ba
noahf909 <112297507+noahf909@users.noreply.github.com>
2024-11-22 19:48:03 -0500

    update vite config to domain

--- AAB/vite.config.ts
+++ AAB/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
       proxy: {
           '/api': {
-              target: 'http://localhost:5000', // Backend server
+              target: 'http://www.aab.run:5000', // Backend server
               changeOrigin: true,
               secure: false,
           },
